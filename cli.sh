b9189f822f67f6e5e71a63f26e791febf352156e
Xie GeXin <2968014018@qq.com>
2025-12-15 14:54:16 +0800

    init commit

---
+++ cli.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+help(){
+  echo "--------------------------------------------------------------------------"
+  echo ""
+  echo "usage: ./cli.sh [updv]"
+  echo ""
+  echo "-updv [version num]   Update all oneblog related versions."
+  echo ""
+  echo "--------------------------------------------------------------------------"
+}
+
+case "$1" in
+  'updv')
+    docs/bin/updVersion.sh $2
+	;;
+  'b')
+    docs/bin/build.sh
+	;;
+  'p')
+    docs/bin/push.sh
+	;;
+  'pd')
+    docs/bin/push-dev.sh
+	;;
+  'dd')
+    read -p '是否重新执行 mvn package？[y/n]' repeat
+    if [ "${repeat}" = "y" -o "${repeat}" = "Y" ];then
+      mvn -X clean package -Dmaven.test.skip=true
+      STATUS=$?
+      if [[ $STATUS == 0 ]]; then
+        docs/bin/deploy-docker.sh
+      else
+        echoError "打包失败"
+        exit 1
+      fi
+    else
+      docs/bin/deploy-docker.sh
+    fi
+	;;
+  'db')
+    read -p '是否重新执行 mvn package？[y/n]' repeat
+    if [ "${repeat}" = "y" -o "${repeat}" = "Y" ];then
+      mvn -X clean package -Dmaven.test.skip=true
+      STATUS=$?
+      if [[ $STATUS == 0 ]]; then
+        docs/bin/build-docker.sh
+      else
+        echoError "打包失败"
+        exit 1
+      fi
+    else
+      docs/bin/build-docker.sh
+    fi
+	;;
+  *)
+    help
+esac
